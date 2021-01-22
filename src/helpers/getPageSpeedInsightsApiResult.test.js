8ba2f523c0a2481b56785381b678f1bfa09bd23d
adamhenson <adamhenson1979@gmail.com>
2021-01-22 17:32:48 -0500

    fix: encode URL to prevent broken API requests

--- src/helpers/getPageSpeedInsightsApiResult.test.js
+++ src/helpers/getPageSpeedInsightsApiResult.test.js
@@ -31,7 +31,7 @@ describe('getPageSpeedInsightsApiResult', () => {
     });
     const response = await getPageSpeedInsightsApiResult(params);
     expect(fetch).toHaveBeenCalledWith(
-      'https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://www.foo.software&category=ACCESSIBILITY&category=BEST_PRACTICES&category=PERFORMANCE&category=PWA&category=SEO&strategy=desktop&key=abc'
+      'https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed?url=https%3A%2F%2Fwww.foo.software&category=ACCESSIBILITY&category=BEST_PRACTICES&category=PERFORMANCE&category=PWA&category=SEO&strategy=desktop&key=abc'
     );
     expect(response).toEqual({ mockResult: true });
   });
